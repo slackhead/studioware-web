b3519a2b54f8fcba12df424c5060afb4751469ce
slackhead <dave@studioware.org>
2018-02-02 18:23:15 +0000

    Update to new-style.css

--- header.php
+++ header.php
@@ -5,15 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="chrome=1">
     <title>Studioware</title>
 
-    <link rel="stylesheet" href="<!--SITEURL-->/stylesheets/site.css">
-    <link rel="stylesheet" href="<!--SITEURL-->/stylesheets/styles.css">
-    <link rel="stylesheet" href="<!--SITEURL-->/stylesheets/pygment_trac.css">
+    <link rel="stylesheet" href="<!--SITEURL-->/stylesheets/new-style.css">
     <link rel="shortcut icon" href="/favicon.ico"/>
-    <script src="<!--SITEURL-->/javascripts/scale.fix.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
   </head>
   <body>
     <div class="wrapper">
@@ -31,17 +24,13 @@
       </header>
       <section>
         <p class="links">
-<a href="http://studioware.org">Home</a> |
-<a href="http://docs.slackware.com/studioware:applications_list">Applications</a> |
-<a href="http://studioware.org/slackbuilds/">SlackBuilds</a> |
-<a href="http://studioware.org/packages/">Packages</a> |
-<a href="http://docs.slackware.com/studioware:start">SlackDocs</a> |
-<a href="http://docs.slackware.com/studioware:quick_start">Quick Start</a> |
-<a href="http://studioware.org/sepkg/">Sepkg</a> |
-<a href="http://studioware.org/iso/">Studioware Live</a> |
-<a href="http://studioware.org/kernels/">Kernels</a> |
-<a href="http://docs.slackware.com/studioware:start">Wiki</a> |
+<a href="http://studioware.org">Home</a>
+<a href="http://docs.slackware.com/studioware:applications_list">Applications</a>
+<a href="http://studioware.org/slackbuilds/">SlackBuilds</a>
+<a href="http://studioware.org/packages/">Packages</a>
+<a href="http://docs.slackware.com/studioware:start">SlackDocs</a>
+<a href="http://studioware.org/sepkg/">Sepkg</a>
+<a href="http://studioware.org/iso/">Studio Live</a>
+<a href="http://studioware.org/kernels/">Kernels</a>
 <a href="http://studioware.org/links">Links</a></p>
-
-    <!--[if !IE]><script>fixScale(document);</script><![endif]-->
 
